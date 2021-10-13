d7bf504746bbf4a52ffc216566bb3293bb37c923
Mohammad <Dilshad.Mohammad@mnscorp.net>
2021-10-13 03:16:34 +0000

    automate chrome browser

---
+++ src/main/java/automation/demo/chrome.java
@@ -0,0 +1,12 @@
+package automation.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class chrome {
+	public static void main(String agrs[]) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\P9169174\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver chrome = new ChromeDriver();
+		chrome.manage().window().maximize();
+	}
+}
